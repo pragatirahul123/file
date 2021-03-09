6a245eecb2a1493e1b5d5bec3b791f340c7d6b6e
zebanavgurukul <zeba18@navgurukul.org>
2021-03-09 23:33:16 +0530

    files

---
+++ File_Qu2.py
@@ -0,0 +1,11 @@
+#var=open("people1-exercise.txt")
+count=0
+for line in (open("people1-exercise.txt").readlines()):
+	count=count+1
+print(count)
+
+
+
+
+
+
---
+++ File_Qu3.py
@@ -0,0 +1,12 @@
+f = ["Kotak", "HDFC", "RBL", "SBI", "Bank of Baroda"]
+with open("File_Ques3.txt", "w") as data:
+	for x in f:
+		data.write(x)
+		data.write("\n")
+
+
+
+
+
+
+
